7a81d9cf6d3c0addb2496e1e6cc87a53bd5861a8
a.bondarenko <andrii.bondarenko@valtech.com>
2021-04-24 16:51:15 +0300

    Add changing Cats

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
+import React, { Component } from "react"
 import Slider from'./components/Slider'
 import './App.css';
-
-function App() {
-  const catImages = [
-    'https://bipbap.ru/wp-content/uploads/2017/12/65620375-6b2b57fa5c7189ba4e3841d592bd5fc1-800-640x426.jpg',
+const imageData = [
+  [
     'https://bipbap.ru/wp-content/uploads/2017/12/iPhone-Merry-Christmas-Cat-Wallpaper-640x361.png',
-    'https://bipbap.ru/wp-content/uploads/2017/12/kittie-animals-blue-cats-eyes-2048x1152-wallpaper-640x360.jpg',
-    'https://bipbap.ru/wp-content/uploads/2017/12/01-vigilant-red-kitten-640x480.jpg',
-    'https://bipbap.ru/wp-content/uploads/2017/12/1200x628_21_6e34306a2db10f61e0f641dd697ad619-1200x630_0x3e4c1b9b_20276859161456653801-640x335.jpeg'
-  ];
+    'https://bipbap.ru/wp-content/uploads/2017/12/iPhone-Merry-Christmas-Cat-Wallpaper-640x361.png',
+    'https://bipbap.ru/wp-content/uploads/2017/12/iPhone-Merry-Christmas-Cat-Wallpaper-640x361.png',
+    'https://bipbap.ru/wp-content/uploads/2017/12/iPhone-Merry-Christmas-Cat-Wallpaper-640x361.png',
+    'https://bipbap.ru/wp-content/uploads/2017/12/iPhone-Merry-Christmas-Cat-Wallpaper-640x361.png',
+  ],
+  [
+    'https://bipbap.ru/wp-content/uploads/2017/12/65620375-6b2b57fa5c7189ba4e3841d592bd5fc1-800-640x426.jpg',
+    'https://bipbap.ru/wp-content/uploads/2017/12/65620375-6b2b57fa5c7189ba4e3841d592bd5fc1-800-640x426.jpg',
+    'https://bipbap.ru/wp-content/uploads/2017/12/65620375-6b2b57fa5c7189ba4e3841d592bd5fc1-800-640x426.jpg',
+    'https://bipbap.ru/wp-content/uploads/2017/12/65620375-6b2b57fa5c7189ba4e3841d592bd5fc1-800-640x426.jpg',
+    'https://bipbap.ru/wp-content/uploads/2017/12/65620375-6b2b57fa5c7189ba4e3841d592bd5fc1-800-640x426.jpg'
+  ]
+]
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="App">
-        <Slider images={catImages}/>
-    </div>
-  );
+    this.state = {
+      images: imageData[0],
+      index: 0
+    };
+  }
+
+  changeCats() {
+    let index = 0;
+
+    if (this.state.index === 0) {
+     index = 1;
+    }
+
+    const newState = {
+      index: index,
+      images: imageData[index]
+    };
+
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Slider images={this.state.images}/>
+        <button onClick={this.changeCats.bind(this)}>Change cats</button>
+      </div>
+    );
+  }
 }
 
 export default App;
